2fda885b3f7ca11a2de8debb259ed3c9a4b59949
123Yasas <yasaswini.23ads@sonatech.ac.in>
2026-02-18 22:19:45 +0530

    Implement loading screen and add styled-components for styling

--- App.tsx
+++ App.tsx
@@ -1,28 +1,92 @@
-
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import Committee from './pages/Committee';
-import Events from './pages/Events';
-import Gallery from './pages/Gallery';
-import Membership from './pages/Membership';
-import Contact from './pages/Contact';
-import AdminDashboard from './pages/AdminDashboard';
-import About from './pages/About';
+import React, { useState, useEffect } from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Loader from "./components/Loader";
+import Home from "./pages/Home";
+import Committee from "./pages/Committee";
+import Events from "./pages/Events";
+import Gallery from "./pages/Gallery";
+import Membership from "./pages/Membership";
+import Contact from "./pages/Contact";
+import AdminDashboard from "./pages/AdminDashboard";
+import About from "./pages/About";
 
 const App: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate page loading time
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // 2 seconds loading
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <Router>
       <Routes>
         {/* Main Site Routes */}
-        <Route path="/" element={<Layout><Home /></Layout>} />
-        <Route path="/about" element={<Layout><About /></Layout>} />
-        <Route path="/committee" element={<Layout><Committee /></Layout>} />
-        <Route path="/events" element={<Layout><Events /></Layout>} />
-        <Route path="/gallery" element={<Layout><Gallery /></Layout>} />
-        <Route path="/membership" element={<Layout><Membership /></Layout>} />
-        <Route path="/contact" element={<Layout><Contact /></Layout>} />
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <Layout>
+              <About />
+            </Layout>
+          }
+        />
+        <Route
+          path="/committee"
+          element={
+            <Layout>
+              <Committee />
+            </Layout>
+          }
+        />
+        <Route
+          path="/events"
+          element={
+            <Layout>
+              <Events />
+            </Layout>
+          }
+        />
+        <Route
+          path="/gallery"
+          element={
+            <Layout>
+              <Gallery />
+            </Layout>
+          }
+        />
+        <Route
+          path="/membership"
+          element={
+            <Layout>
+              <Membership />
+            </Layout>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <Layout>
+              <Contact />
+            </Layout>
+          }
+        />
 
         {/* Admin Routes */}
         <Route path="/admin" element={<AdminDashboard />} />
